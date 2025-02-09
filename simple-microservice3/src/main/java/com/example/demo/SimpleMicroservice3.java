1a0b3a5277cfb7b72e6f1539ba79f7df2ddd54ea
Keith <kcrowe123@yahoo.com>
2025-02-08 20:54:58 -0600

    Process of converting to maven

--- simple-microservice3/src/main/java/com/example/demo/SimpleMicroservice3.java
+++ simple-microservice3/src/main/java/com/example/demo/SimpleMicroservice3.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SimpleMicroservice3 {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SimpleMicroservice3.class, args);
     }
 
 }
