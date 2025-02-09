bbf8cda22b44a1124646ab56c97c8144dba03e49
Keith Crowe <Keith.J.Crowe@ehi.com>
2025-02-09 13:35:45 -0600

    Adds Crud capability

--- simple-microservice2/src/main/java/com/example/demo/rest/HelloController.java
+++ simple-microservice2/src/main/java/com/example/demo/rest/HelloController.java
@@ -1,9 +1,18 @@
 package com.example.demo.rest;
 
+import com.example.demo.entity.Greeting;
 import com.example.demo.service.HelloService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,7 +20,30 @@ public class HelloController {
     private final HelloService helloService;
 
     @GetMapping
-    public String hello() {
-        return helloService.hello();
+    public List<Greeting> findAllGreetings() {
+        return helloService.listAllGreetings();
+    }
+
+    @GetMapping("/{id}")
+    public Greeting findById(@PathVariable Long id) {
+        return helloService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody Greeting greeting) {
+        helloService.updateGreetingById(id, greeting);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody Greeting greeting) {
+        helloService.createMessage(greeting);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> create(@PathVariable Long id) {
+        helloService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- simple-microservice2/src/main/java/com/example/demo/service/HelloService.java
+++ simple-microservice2/src/main/java/com/example/demo/service/HelloService.java
@@ -6,18 +6,35 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class HelloService {
     @Autowired
     private GreetingRepository greetingRepository;
 
+    public void updateGreetingById(Long id, Greeting greeting) {
+        Optional<Greeting> byId = greetingRepository.findById(id);
+        Greeting savedGreeting = byId.get();
+        savedGreeting.setMessage(greeting.getMessage());
+        greetingRepository.save(savedGreeting);
+    }
 
-    public String hello() {
-        if(greetingRepository.count() < 1) {
-            var response = greetingRepository.save(new Greeting("Hello"));
-            return response.getMessage();
-        }
-        return greetingRepository.findAll().get(0).getMessage();
+    public void createMessage(Greeting greeting) {
+        greetingRepository.save(greeting);
+    }
+
+    public void deleteById(Long id) {
+        greetingRepository.deleteById(id);
+    }
+
+    public Greeting findById(Long id) {
+        return greetingRepository.findById(id).get();
+    }
+
+    public List<Greeting> listAllGreetings() {
+        return greetingRepository.findAll();
     }
 }
